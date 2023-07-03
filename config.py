9a50e1a9c70efcedce3965eb44e906e48b9300a3
MussabPro <124181205+MussabPro@users.noreply.github.com>
2023-07-03 23:24:53 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "nabfiyeghh978r1y989(&Y*&%#V^&%fgegqw)724637567y86R^E@R^R6"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "Your SECRET KEY"
 
-ADMIN_USERNAME = 'Mussab'
-ADMIN_PASSWORD = '131416'
+ADMIN_USERNAME = 'Username'
+ADMIN_PASSWORD = 'Password'
 
 print(Config.SECRET_KEY)
